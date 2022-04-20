605b949fe402c015fd4c717a161f3c4dd3b4e175
Long Hồ <thienlong460@gmail.com>
2022-04-20 14:09:47 +0700

    +first commit

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import SafeViewAndroid from "./SafeViewAndroid";
 
 export default function App() {
+  console.log("App")
+
+  let x;
+
+  x = 2;
+  x.toString()
+j.
+  acc;j
+;;;l;'k'+k;
+  const handlePress = () => {
+    console.log("Press")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+      <View style={styles.container}>
+        <Text onPress={handlePress}>Hello</Text>
+        {/*
+         <Image source={require('./assets/splash.png')} />
+  
+*/}      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +33,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
